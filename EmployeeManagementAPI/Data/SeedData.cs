63011e9293eaba136f1c5d71eee9c0b1c82534fa
Rohith <rohithneela369@gmail.com>
2026-04-15 15:11:48 +0530

    feat: add seed data and integrate serial log Logging

---
+++ EmployeeManagementAPI/Data/SeedData.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeManagementAPI.Models;
+
+namespace EmployeeManagementAPI.Data
+{
+    public static class SeedData
+    {
+        public static void Initialize(AppDbContext context)
+        {
+            if (context.Employees.Any()) return;
+
+            var employees = new List<Employee>
+            {
+                new() { Name="Rohith", Email="rohith@ems.com", Department="IT", Designation="Developer", Salary=50000, JoinedDate=DateTime.Now },
+                new() { Name="Priya", Email="priya@ems.com", Department="HR", Designation="HR Manager", Salary=45000, JoinedDate=DateTime.Now },
+                new() { Name="Suresh", Email="suresh@ems.com", Department="Finance", Designation="Analyst", Salary=40000, JoinedDate=DateTime.Now },
+                new() { Name="Anitha", Email="anitha@ems.com", Department="IT", Designation="QA Engineer", Salary=42000, JoinedDate=DateTime.Now },
+                new() { Name="Kiran", Email="kiran@ems.com", Department="IT", Designation="Senior Developer", Salary=70000, JoinedDate=DateTime.Now },
+                new() { Name="Meena", Email="meena@ems.com", Department="HR", Designation="Recruiter", Salary=38000, JoinedDate=DateTime.Now },
+                new() { Name="Vijay", Email="vijay@ems.com", Department="Finance", Designation="Accountant", Salary=36000, JoinedDate=DateTime.Now },
+                new() { Name="Deepa", Email="deepa@ems.com", Department="IT", Designation="DevOps Engineer", Salary=65000, JoinedDate=DateTime.Now },
+                new() { Name="Ravi", Email="ravi@ems.com", Department="IT", Designation="Tech Lead", Salary=85000, JoinedDate=DateTime.Now },
+                new() { Name="Lakshmi", Email="lakshmi@ems.com", Department="HR", Designation="HR Director", Salary=90000, JoinedDate=DateTime.Now }
+            };
+
+            context.Employees.AddRange(employees);
+            context.SaveChanges();
+        }
+    }
+}
--- EmployeeManagementAPI/Program.cs
+++ EmployeeManagementAPI/Program.cs
@@ -3,44 +3,73 @@ using EmployeeManagementAPI.Services;
 using EmployeeManagementAPI.Middleware;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using Serilog;
 
 namespace EmployeeManagementAPI
 {
     public class Program
     {
         public static void Main(string[] args)
+
         {
-            var builder = WebApplication.CreateBuilder(args);
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("Logs/app-log-.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
 
-            // Add services to the container.
-            builder.Services.AddControllers();
-            builder.Services.AddScoped<IEmployeeService,EmployeeService>();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-            // test change
-            builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            try
             {
-                app.MapOpenApi();
-                app.MapScalarApiReference();
+                Log.Information("Starting EmployeeManagementAPI....");
+
+                var builder = WebApplication.CreateBuilder(args);
+
+                builder.Host.UseSerilog();
+
+                // Add services to the container.
+                builder.Services.AddControllers();
+                builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+                // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+                builder.Services.AddOpenApi();
+                // test change
+                builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+                var app = builder.Build();
+
+                using (var scope = app.Services.CreateScope())
+                {
+                    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                    SeedData.Initialize(context);
+                }
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.MapOpenApi();
+                    app.MapScalarApiReference();
+                }
+                app.UseMiddleware<ExceptionMiddleware>();
+                app.UseMiddleware<RequestLoggingMiddleware>();
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+                app.MapControllers();
+
+                app.Run();
             }
-            app.UseMiddleware<ExceptionMiddleware>();
-            app.UseMiddleware<RequestLoggingMiddleware>();
+            catch (Exception ex)
+            {
 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+                Log.Fatal(ex, "Application failed to start");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
