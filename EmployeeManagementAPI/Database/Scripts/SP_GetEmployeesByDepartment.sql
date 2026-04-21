24232a36e8122f3120f5d73a873c5bdfc403fe6a
Rohith <rohithneela369@gmail.com>
2026-04-21 12:07:22 +0530

    feat:add Sql scripts and stored procedures

---
+++ EmployeeManagementAPI/Database/Scripts/SP_GetEmployeesByDepartment.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE SP_GetEmployeesByDepartment
+    @Department NVARCHAR(100)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT Id, Name, Email, Department, Designation, Salary, JoinedDate
+    FROM Employees
+    WHERE Department = @Department
+    ORDER BY Name ASC
+END
